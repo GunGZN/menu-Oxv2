0e435ee93700cbf7681983f79d186eec27920de0
GunGZN <86140154+GunGZN@users.noreply.github.com>
2021-09-29 01:26:17 +0700

    Create rootpass.sh

---
+++ rootpass.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+red='\e[1;31m'
+green='\e[0;32m'
+NC='\e[0m'
+MYIP=$(wget -qO- icanhazip.com);
+clear
+echo -e ""
+echo -e "**************************************"
+echo -e "             ทำตามขั้นตอน 1-2 "
+echo -e ""
+echo -e "     [1]  ตั้งรหัสสำหรับ ROOT (U16-18)"
+echo -e "     [2]  เปิดการอนุญาติ ROOT LOGIN (ต้องเลือก)"
+echo -e "     [x]  ออก"
+echo -e "**************************************"
+echo -e ""
+read -p "    กรุณาเลือก [1,2 หรือ x] :  " rootpass
+echo -e ""
+echo -e "**************************************"
+sleep 1
+case $rootpass in   
+                1)
+                clear
+                echo -e "**************************************"
+                echo -e ""
+                echo -e "          กรุณาใส่รหัสผ่านที่ต้องการ "
+                echo -e ""
+                echo -e "**************************************"
+                sudo passwd
+                sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+                rootpass
+                ;;
+                2)
+                sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+                sudo service ssh restart
+                exit
+                ;;
+                3)
+                sudo service ssh restart
+                echo -e ""
+                echo -e "**************************************"
+                echo -e ""
+                echo -e "          ดำเนินการสำเร็จ "
+                echo -e ""
+                echo -e "**************************************"
+                menu
+                ;;
+                x)
+                clear
+                menu
+                ;;
+        esac
